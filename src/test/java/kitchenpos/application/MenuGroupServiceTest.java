ced8faba340bec738265e92491cdee6c5b246361
wotjd4305 <43901682+wotjd4305@users.noreply.github.com>
2025-02-05 23:50:48 +0900

    test : (Kitchenpos) step3. 메뉴 그룹 테스트 케이스 추가

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,70 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.fixture.MenuGroupFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class MenuGroupServiceTest {
+
+    private MenuGroupRepository menuGroupRepository;
+    private MenuGroupService menuGroupService;
+
+    @BeforeEach
+    void setUp() {
+        menuGroupRepository = mock(MenuGroupRepository.class);
+        menuGroupService = new MenuGroupService(menuGroupRepository);
+    }
+
+    @DisplayName("메뉴 그룹명은 비어있으면 안된다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void create_name_empty(final String name) {
+        MenuGroup menuGroup = MenuGroupFixture.createMenuGroup(name);
+
+        when(menuGroupRepository.save(any(MenuGroup.class))).thenReturn(menuGroup);
+
+        assertThatThrownBy(() -> menuGroupService.create(menuGroup))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("새로운 메뉴 그룹을 생성할 수 있다.")
+    @ParameterizedTest(name = "메뉴 그룹명 : `{0}`")
+    @ValueSource(strings = {"추천 메뉴", "사이드 메뉴", "포장 메뉴"})
+    void create(final String name) {
+        MenuGroup menuGroup = MenuGroupFixture.createMenuGroup(name);
+
+        when(menuGroupRepository.save(any(MenuGroup.class))).thenReturn(menuGroup);
+        MenuGroup actual = menuGroupService.create(menuGroup);
+
+        assertThat(actual).isEqualTo(menuGroup);
+    }
+
+    @DisplayName("메뉴 그룹 전부 가져올 수 있다.")
+    @Test
+    void findAll() {
+        MenuGroup menuGroup = MenuGroupFixture.createMenuGroup("추천메뉴");
+        when(menuGroupRepository.findAll()).thenReturn(List.of(menuGroup));
+        List<MenuGroup> result = menuGroupService.findAll();
+
+        assertAll(
+                () -> assertThat(result).isNotEmpty(),
+                () -> assertEquals(result.size(), 1)
+        );
+    }
+}
